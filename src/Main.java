7a6b576f226c09a2f6890dcc923f14f7fd75246c
Eldana <eldana.toktosartova@alatoo.edu.kg>
2024-10-24 13:18:22 +0600

    Methods to randomly hide places of ships and print empty field added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
+import java.util.Random;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static int size = 7;
+    private static char emptyField = '.';
+    private static char ship = '❌';
+    private static char[][] field = new char[size][size];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hi! " +
-                "\n" + "It is a Modified Sea Battle game. Enter your nickname: ");
-        String name = sc.nextLine();
+
+        System.out.println("Hi!" +
+                "\n" + "It is a Modifies Sea Battle game." +
+                "\n" + "Enter your nickname: ");
+
+        String nickname = sc.nextLine();
+        System.out.println("Welcome, " + nickname + "!");
+
+        System.out.println("Enter coordinates of the ship: ");
 
     }
-}
+
+    static void cleanAll() {
+        System.out.println("\033[H\033[J");
+    }
+
+    static int printField(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                field[i][j] = emptyField;
+            }
+        }
+    return 0;}
+
+    static int placeShip(int size) {
+        Random rand = new Random();
+        boolean place = false;
+
+        while (!place) {
+            boolean horizontal = rand.nextBoolean();
+            int row = rand.nextInt(size);
+            int col = rand.nextInt(size);
+
+                for (int i = 0; i < row; i++) {
+                    if (horizontal) {
+                        field[row][col + i] = ship;
+                    } else {
+                        field[row + i][col] = ship;
+                    }
+                }
+                place = true;
+            }
+        return size;}
+    }
